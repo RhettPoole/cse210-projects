f3d5a8f37fea9d7859ee7b2af5f7c294aadba394
rhettpoole20@byui.edu <rhettpoole.20@gmail.com>
2023-04-21 20:27:40 -0600

    Prep 1, Variables and Input/Output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /*
+        Console.WriteLine("Hello Prep1 World! ");
+        Console.WriteLine("This is in C#.");
+        
+
+        int number = 5;
+        number = 7;
+
+                How to take input from the user
+        Console.WriteLine("What is your favorite number?");
+                "Console.Write" -> No newline after writeline
+        string favorite_number = Console.ReadLine();
+                Use "$" instead of "f" for interpolation 
+        Console.WriteLine($"Your favorite number is: {favorite_number}");
+        
+
+                /* Prompt the user for their first and last name */
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+                /* Print the James Bond Quote using the input. */
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
         Console.WriteLine("Hello Prep2 World!");
+        */
+        /*
+        Console.Write("Enter number: ");
+        string valueFromUser = Console.ReadLine();
+        */
+            /* Use "int.Parse" to convert string to int, use "{value-name}.ToString()" to convert to a string. 
+        int x =  int.Parse(valueFromUser);
+        int y = 2;
+        
+            /*
+                || = or
+                && = and
+            
+
+        if (x > y)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x == y)
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not Greater");
+        }
+        */
+
+
+            /*""" Write a program that will determine letter grades """*/
+
+        /* Get input from the user */
+        Console.Write("What is your grade percentage?: ");
+        string percentage = Console.ReadLine();
+
+        /* Convert input to an integer, I'm just doing this for practice using "int.Parse" */
+        int grade = int.Parse(percentage);
+
+        /* If statement to determine if the student passed. */
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed, better luck next time partner.");
+        }
+
+        /* Create the "letter" variable */
+        string letter = "";
+
+        /* If statement to determine letter grade */
+        if (grade >= 90)
+        {
+            letter = ("A");
+        }
+        else if (grade <= 90 && grade >= 80)
+        {
+            letter = ("B");
+        }
+        else if (grade <= 80 && grade >= 70)
+        {
+            letter = ("C");
+        }
+        else if (grade <= 70 && grade >= 60)
+        {
+            letter = ("D");
+        }
+        else
+        {
+            letter = ("F");
+        }
+
+        Console.WriteLine($"{letter}");
     }
 }
