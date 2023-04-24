5353a1c0ce1b5dbba8d511d080ff87ec68c2051e
rhettpoole20@byui.edu <rhettpoole.20@gmail.com>
2023-04-24 17:56:09 -0600

    Prep #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
+        /*
         Console.WriteLine("Hello Prep3 World!");
+
+        for (int i = 2; i <= 20; i++)
+        {
+            Console.WriteLine(i);
+        }
+        */
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
